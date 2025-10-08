7cd369119bb0f8986a79aa73e0d82afe4689e9d5
Otutochi Nwadinkpa <o.caleb.nwa@gmail.com>
2025-10-07 19:28:30 -0500

    added get event by id to controller

--- server/controllers/events.js
+++ server/controllers/events.js
@@ -1,10 +1,21 @@
 import { pool } from "../config/database.js"
 
 const getEvents = async (req, res) => {
-    const { location_id } = req.query
+    const { event_id, location_id } = req.query
 
     try {
-        if (location_id !== undefined) {
+        if (event_id !== undefined) {
+            const id = Number(event_id)
+            if(Number.isNaN(id)) {
+                return res.status(400).json({ error: 'event_id must be an integer' })
+            }
+
+            const query = 'SELECT id, name, location_id, date_time, image_link FROM events WHERE id = $1'
+            const values = [id]
+
+            const result = await pool.query(query, values)
+            return res.status(200).json(result.rows[0])
+        } else if (location_id !== undefined) {
             const id = Number(location_id)
             if (Number.isNaN(id)) {
                 return res.status(400).json({ error: 'location_id must be an integer' })
